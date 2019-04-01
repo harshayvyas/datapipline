79d4ea94aa461e21ed704b80d039f7c05bc7fc8c
ADMIN <vyasharshay.hv@gmail.com>
2019-04-01 17:39:20 +0530

    oracle files

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,13 @@
 import boto3
+import cx_Oracle
 
 def lambda_handler(event,context):
     print("hello world")
+    print(cx_Oracle.clientversion())
+
+
+"""
+if __name__=="__main__":
+    l=lambda_handler('event','context')
+    print(l)
+"""
